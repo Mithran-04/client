d8ad0a8369e5f51c0f9a81f88781b67142c5b963
Namee <nameem.gmail.com>
2023-08-30 02:10:32 +0530

    modified:   package.json modified:   src/App.css modified:   src/App.js 	new file:   src/Editor.js 	new file:   src/Header.js 	new file:   src/Layout.js 	new file:   src/Post.js 	new file:   src/UserContext.js 	modified:   src/index.js 	new file:   src/pages/CreatePost.js 	new file:   src/pages/EditPost.js 	new file:   src/pages/LoginPage.js 	new file:   src/pages/PostPage.js 	new file:   src/pages/RegisterPage.js 	new file:   src/pages/indexPage.js 	modified:   yarn.lock

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Post from "./Post";
+import Header from "./Header";
+import { Route, Routes } from "react-router-dom";
+import Layout from "./Layout";
+import IndexPage from "./pages/indexPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import { UserContextProvider } from "./UserContext";
+import CreatePost from "./pages/CreatePost";
+import PostPage from "./pages/PostPage";
+import EditPost from "./pages/EditPost";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <UserContextProvider>
+      <Routes>
+        <Route path="/" element={<Layout></Layout>}>
+          <Route index element={<IndexPage></IndexPage>}></Route>
+
+          <Route path="/login" element={<LoginPage></LoginPage>}></Route>
+          <Route
+            path="/register"
+            element={<RegisterPage></RegisterPage>}
+          ></Route>
+          <Route path="/create" element={<CreatePost></CreatePost>}></Route>
+          <Route path="/post/:id" element={<PostPage></PostPage>}></Route>
+          <Route path="/edit/:id" element={<EditPost></EditPost>}></Route>
+        </Route>
+      </Routes>
+    </UserContextProvider>
   );
 }
 
---
+++ src/pages/CreatePost.js
@@ -0,0 +1,83 @@
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import { Navigate } from "react-router-dom";
+import { useState } from "react";
+const modules = {
+  toolbar: [
+    [{ header: [1, 2, false] }],
+    ["bold", "italic", "underline", "strike", "blockquote"],
+    [
+      { list: "ordered" },
+      { list: "bullet" },
+      { indent: "-1" },
+      { indent: "+1" },
+    ],
+    ["link", "image"],
+    ["clean"],
+  ],
+};
+
+const formats = [
+  "header",
+  "bold",
+  "italic",
+  "underline",
+  "strike",
+  "blockquote",
+  "list",
+  "bullet",
+  "indent",
+  "link",
+  "image",
+];
+
+export default function CreatePost() {
+  const [title, setTitle] = useState("");
+  const [summary, setSummary] = useState("");
+  const [content, setContent] = useState("");
+  const [files,setFiles]=useState('');
+  const[redirect,setRedirect]=useState(false);
+  async function createNewPost(ev){
+    const data=new FormData();
+    data.set('title',title)
+    data.set('summary',summary)
+    data.set('content',content)
+    data.set('file',files[0]);
+    ev.preventDefault();
+    const response=await fetch('http://localhost:4000/post',{
+        method:'POST',
+        body:data,
+        credentials:'include',
+        // We are using credentials include because , by this we can send the cookie we created that contains current user information
+      });
+
+    if(response.ok){
+          setRedirect(true);
+    }
+    response.json()
+
+  }
+  if(redirect){
+    return <Navigate to ={'/'}></Navigate>
+  }
+  return (
+    <form onSubmit={createNewPost}>
+      <input type="title" placeholder={"Title"} value={title} onChange={ev=>
+        setTitle(ev.target.value)}></input>
+      <input type="summary" placeholder={"Summary"}
+        value={summary}
+        onChange={ev=>setSummary(ev.target.value)}></input>
+      
+      <input type="file" 
+      onChange={ev=>setFiles(ev.target.files)}></input>
+      <ReactQuill
+        value={content}
+        onChange={newValue=>setContent(newValue)}
+        modules={modules}
+        formats={formats}
+      ></ReactQuill>
+
+      <button style={{ marginTop: "5px" }}>Create post</button>
+    </form>
+  );
+}
